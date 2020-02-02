47a258d619dc27d59b0eb733f2bceb064d073ad7
Unknown <if708833@iteso.mx>
2020-02-02 17:03:04 -0600

    Finales codigo

--- Laboratorio 0.py
+++ Laboratorio 0.py
@@ -10,6 +10,8 @@ import funciones as fn
 import visualizaciones as vs
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+
 # -- --------------------------------------------------------- Descargar precios de OANDA -- #
 # Este código nos permite descargar los precios según los parámetros que nosotros indiquemos con la ayuda de la herramienta OANDA
 # token de OANDA
@@ -30,16 +32,16 @@ vs_grafica1 = vs.g_velas(p0_de=df_pe.iloc[0:120, :])
 # -- ------------------------------------------------------------------- Conteno de velas -- #
 
 # -- 0A.1: Hora
-df_pe['hora'] = list(df_pe['TimeStamp'][i].hour for i in range(0, len(df_pe['TimeStamp'])))  # nueva columna de hora
+df_pe['Hora'] = list(df_pe['TimeStamp'][i].hour for i in range(0, len(df_pe['TimeStamp'])))  # nueva columna de hora
 # -- 0A.2: Dia de la semana.
-df_pe['dia'] = list(df_pe['TimeStamp'][i].weekday() for i in range(0, len(df_pe['TimeStamp'])))
+df_pe['Dia'] = list(df_pe['TimeStamp'][i].weekday() for i in range(0, len(df_pe['TimeStamp'])))
 # -- CO: Amplitud de velas (close - open).
 closes = pd.DataFrame(float(i) for i in df_pe['Close'])
 opens = pd.DataFrame(float(i) for i in df_pe['Open'])
-df_pe['co'] = (closes-opens)*10000
+df_pe['CO'] = (closes-opens)
 # -- ---------------------------------Ejercicios individuales laboratorio 0 -- #
 # -- 01: Mes de la vela.
-df_pe['mes'] = pd.DataFrame(i.month for i in df_pe['TimeStamp'])
+df_pe['Mes'] = pd.DataFrame(i.month for i in df_pe['TimeStamp'])
 
 # -- 02: Sesion de la vela.
 # Este código nos permite identificar la sesión a la que pertenece cada vela según a la hora en la que sucedió el movimiento
@@ -59,18 +61,30 @@ def apply_condiciones(i):
     return i
 
 # Por último creamos la columna de sesiones y asignamos los datos obtenidos en base a la función creada
-df_pe['sesion'] = list(map(apply_condiciones, df_pe['hora']))
+df_pe['Sesion'] = list(map(apply_condiciones, df_pe['Hora']))
 
 # -- 03: Amplitud OC esperada de vela para cualquier dia de la semana (Dist de Freq).
-df_pe['oc'] = (closes-opens)*10000
+df_pe['OC'] = (closes-opens)*10000
 # -- 04: Amplitud HL esperada de vela para cualquier dia de la semana (Dist de Freq).
 high = pd.DataFrame(float(i) for i in df_pe['High'])
 low = pd.DataFrame(float(i) for i in df_pe['Low'])
-df_pe['hl'] = (high-low)*10000
+df_pe['HL'] = (high-low)*10000
 # -- 05: Sentido de la vela (Alcista o Bajista)
 sentido = lambda opens, closes: 'ALCISTA' if closes>=opens else 'BAJISTA'
-df_pe['sentido'] = pd.DataFrame(sentido(df_pe['Open'][i],df_pe['Close'][i]) for i in range(len(df_pe['Open'])))
+df_pe['Sentido'] = pd.DataFrame(sentido(df_pe['Open'][i],df_pe['Close'][i]) for i in range(len(df_pe['Open'])))
 
-# -- 06: Conteo de velas consecutivas alcistas/bajistas.
 # -- 07: Ventanas móviles de volatilidad
+# En este caso utilizamos la función rolling que nos permite elegir una ventana móvil con cierto número de muestras para posteriormente
+# aplicar alguna medida estadística, en este caso utilizamos la función max para obtener el máximo valor de la ventana móvil indicada
+df_pe['volatilidad_5'] = (df_pe['HL'].rolling(5).max())
+df_pe['volatilidad_25'] = (df_pe['HL'].rolling(25).max())
+df_pe['volatilidad_50'] = (df_pe['HL'].rolling(50).max())
+
 # -- 08: Gráfica con Plotly
+c = df_pe.groupby(['Dia'])['volatilidad_5'].mean()
+import plotly.express as px
+fig = px.scatter(x=[0,1,2,3,4,6], y=c, labels={'x':'Dia de la semana', 'y':'Promedio volatilidad (5)'},
+                 title='Volatilidad en los días de la semana')
+fig.show()
+
+
